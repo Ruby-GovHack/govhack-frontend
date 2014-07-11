ff364e918e90a4c08ecfa7a74b90cfe596b17de8
Alex <lxronald@gmail.com>
2014-07-12 09:35:33 +1000

    Update sites API, added caching.

--- code/app/scripts/controllers/alex.js
+++ code/app/scripts/controllers/alex.js
@@ -2,15 +2,15 @@
 
 angular.module('govhackFrontendApp')
   .controller('AlexCtrl', function ($scope, data) {
-    $scope.sites = data.sites.query();
-    
-    $scope.temps = data.temps.query({
-      timeperiod: 'monthly', // options 'daily', 'monthly', 'yearly'
-      maxormin: 'max', // options 'max', 'min'
-      time: 200809, // YYYYMMDD depending on timeperiod
-      north: -35.0,
-      east: 150.5,
-      south: -36.0,
-      west: 147.2
-    });
+    $scope.sites = data.getSites({ dataset: 'acorn-sat' });
+
+    // $scope.temps = data.temps.query({
+    //   timeperiod: 'monthly', // options 'daily', 'monthly', 'yearly'
+    //   maxormin: 'max', // options 'max', 'min'
+    //   time: 200809, // YYYYMMDD depending on timeperiod
+    //   north: -35.0,
+    //   east: 150.5,
+    //   south: -36.0,
+    //   west: 147.2
+    // });
   });
--- code/app/scripts/services/data.js
+++ code/app/scripts/services/data.js
@@ -5,37 +5,53 @@
  */
 angular.module('govhackFrontendApp')
   .factory('data', function ($resource) {
-    var backendBaseUrl = 'http://api.rubygovhackers.org';
+    var backendBaseUrl = 'http://api.rubygovhackers.org/v1';
+
+    var sitesResource = $resource(
+      backendBaseUrl + '/:dataset/sites',
+      {},
+      {
+        list: {
+          method : 'GET',
+          cache : true
+        }
+      }
+    );
 
     return {
-      // Usage:
-      //   sites.query()
+      // Get sites. Results are cached.
+      // Example usage:
+      //   getSites({ dataset: 'acorn-sat' })
       // Returns:
-      // [
-      //     {"id": "069018", "site": "Moruya Heads", "lat": -35.909, "long": 150.153 },
-      //     {"id": "070351", "site": "Canberra", "lat": -35.309, "long": 149.2 },
-      //     {"id": "072150", "site": "Wagga Wagga", "lat": -35.158, "long": 147.457 },
-      //     {"id": "072161", "site": "Cabramurra", "lat": -35.937, "long": 148.378 }
-      // ]
-      sites: $resource(backendBaseUrl + '/acorn-sat/v1/sites'),
+      //   {
+      //     "069018": { "label": "Moruya Heads", "lat": -35.909, "long": 150.153 },
+      //     "070351": { "label": "Canberra", "lat": -35.309, "long": 149.2 },
+      //     "072150": { "label": "Wagga Wagga", "lat": -35.158, "long": 147.457 },
+      //     "072161": { "label": "Cabramurra", "lat": -35.937, "long": 148.378 }
+      //   }
+      getSites: function(params) {
+        return sitesResource.get(params);
+      },
 
-      // Usage:
+      // Example usage:
       //   temps.query({
       //     timeperiod: 'monthly', // options 'daily', 'monthly', 'yearly'
-      //     maxormin: 'max', // options 'max', 'min'
-      //     time: 200809, // YYYYMMDD depending on timeperiod
+      //     dataset: 'acorn-sat'
+      //     // any combination of 'min-temp', 'max-temp', 'mean-temp' seperated by '+'
+      //     var: 'min-temp+max-temp+mean-temp',
+      //     time: '200809', // YYYYMMDD depending on timeperiod
       //     north: -35.0,
       //     east: 150.5,
       //     south: -36.0,
       //     west: 147.2
       //   })
       // Returns:
-      // [
+      //   [
       //     {"id": "069018", "max":20.02, "std-dev": 5.11 },
       //     {"id": "070351", "max":18.48, "std-dev": 3.95 },
       //     {"id": "072150", "max":20.32, "std-dev": 3.95 },
       //     {"id": "072161", "max":9.61, "std-dev": 3.62 }
-      // ]
-      temps: $resource(backendBaseUrl + '/:timeperiod/acorn-sat/v1/:maxormin-temp')
+      //   ]
+      timeseries: $resource(backendBaseUrl + '/:dataset/:timeperiod/:vars')
     };
   });
