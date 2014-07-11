1226e72a82b750e877ebb7d560f4e577def782e1
Alex <lxronald@gmail.com>
2014-07-11 22:46:41 +1000

    A couple of data endpoints added.

--- code/app/scripts/controllers/alex.js
+++ code/app/scripts/controllers/alex.js
@@ -2,5 +2,15 @@
 
 angular.module('govhackFrontendApp')
   .controller('AlexCtrl', function ($scope, data) {
-    $scope.test = data.someMethod();
+    $scope.sites = data.sites.query();
+    
+    $scope.temps = data.temps.query({
+      timeperiod: 'monthly', // options 'daily', 'monthly', 'yearly'
+      maxormin: 'max', // options 'max', 'min'
+      time: 200809, // YYYYMMDD depending on timeperiod
+      north: -35.0,
+      east: 150.5,
+      south: -36.0,
+      west: 147.2
+    });
   });
--- code/app/scripts/services/data.js
+++ code/app/scripts/services/data.js
@@ -4,16 +4,38 @@
  * Use this service to access the backend data.
  */
 angular.module('govhackFrontendApp')
-  .factory('data', function () {
-    // Service logic
-    // ...
+  .factory('data', function ($resource) {
+    var backendBaseUrl = 'http://api.rubygovhackers.org';
 
-    var meaningOfLife = 42;
-
-    // Public API here
     return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
+      // Usage:
+      //   sites.query()
+      // Returns:
+      // [
+      //     {"id": "069018", "site": "Moruya Heads", "lat": -35.909, "long": 150.153 },
+      //     {"id": "070351", "site": "Canberra", "lat": -35.309, "long": 149.2 },
+      //     {"id": "072150", "site": "Wagga Wagga", "lat": -35.158, "long": 147.457 },
+      //     {"id": "072161", "site": "Cabramurra", "lat": -35.937, "long": 148.378 }
+      // ]
+      sites: $resource(backendBaseUrl + '/acorn-sat/v1/sites'),
+
+      // Usage:
+      //   temps.query({
+      //     timeperiod: 'monthly', // options 'daily', 'monthly', 'yearly'
+      //     maxormin: 'max', // options 'max', 'min'
+      //     time: 200809, // YYYYMMDD depending on timeperiod
+      //     north: -35.0,
+      //     east: 150.5,
+      //     south: -36.0,
+      //     west: 147.2
+      //   })
+      // Returns:
+      // [
+      //     {"id": "069018", "max":20.02, "std-dev": 5.11 },
+      //     {"id": "070351", "max":18.48, "std-dev": 3.95 },
+      //     {"id": "072150", "max":20.32, "std-dev": 3.95 },
+      //     {"id": "072161", "max":9.61, "std-dev": 3.62 }
+      // ]
+      temps: $resource(backendBaseUrl + '/:timeperiod/acorn-sat/v1/:maxormin-temp')
     };
   });
